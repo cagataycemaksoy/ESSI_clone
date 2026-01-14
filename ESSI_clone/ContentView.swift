e7076bb9696605664f01dfa3893cc63a0508a4b9
cagataycemaksoy <cemaksoy7890@gmail.com>
2026-01-14 11:55:26 +0100

    Initial Commit

---
+++ ESSI_clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ESSI_clone
+//
+//  Created by Cem Aksoy on 14.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ESSI_clone/ESSI_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESSI_cloneApp.swift
+//  ESSI_clone
+//
+//  Created by Cem Aksoy on 14.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct ESSI_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
